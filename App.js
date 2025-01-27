97692f1e3ef018832227d351f52b5177b2d902d3
Reima Riihimäki <reimarii@oamk.fi>
2025-01-27 08:51:47 +0200

    Version update

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import {  View} from 'react-native';
 import Styles from './styles/Styles.js';
-import {MD3LightTheme, Provider, Text, Button, TextInput} from 'react-native-paper';
+import {MD3LightTheme, PaperProvider, Text, Button, TextInput} from 'react-native-paper';
 
 
 export default function App() {
